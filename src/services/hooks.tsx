d182d8888a88451bd2a87e6c0b02f73fa7a181b5
Daniil Fomenko <danil.fomchik@gmail.com>
2025-04-02 14:39:48 +0300

    changed Modal component to use useBodyClick hook

--- src/services/hooks.tsx
+++ src/services/hooks.tsx
@@ -1,68 +1,106 @@
-import {useState, useEffect, useRef} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
+
+import {decreaseOpenedItems, increaseOpenedItems} from '@/redux/overflow/overflowSlice';
+import {useAppDispatch} from '@/redux/store';
 
 export const useScreenSize = () => {
     const [screenSize, setScreenSize] = useState<'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | ''>('');
 
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth < 640) {
-                setScreenSize('xs');
-            } else if (window.innerWidth >= 640 && window.innerWidth < 768) {
-                setScreenSize('sm');
-            } else if (window.innerWidth >= 768 && window.innerWidth < 1024) {
-                setScreenSize('md');
-            } else if (window.innerWidth >= 1024 && window.innerWidth < 1280) {
-                setScreenSize('lg');
-            } else if (window.innerWidth >= 1280 && window.innerWidth < 1536) {
-                setScreenSize('xl');
-            } else if (window.innerWidth >= 1536) {
-                setScreenSize('2xl');
-            }
-        };
-
-        window.addEventListener('resize', handleResize);
-        handleResize();
-
-        return () => window.removeEventListener('resize', handleResize);
+    const handleResize = useCallback(() => {
+        if (window.innerWidth < 640) {
+            setScreenSize('xs');
+        } else if (window.innerWidth >= 640 && window.innerWidth < 768) {
+            setScreenSize('sm');
+        } else if (window.innerWidth >= 768 && window.innerWidth < 1024) {
+            setScreenSize('md');
+        } else if (window.innerWidth >= 1024 && window.innerWidth < 1280) {
+            setScreenSize('lg');
+        } else if (window.innerWidth >= 1280 && window.innerWidth < 1536) {
+            setScreenSize('xl');
+        } else if (window.innerWidth >= 1536) {
+            setScreenSize('2xl');
+        }
     }, []);
+
+    useEffect(() => {
+        const observer = new ResizeObserver(handleResize);
+        if (document.body) observer.observe(document.body);
+
+        return () => observer.disconnect();
+    }, [handleResize]);
 
     return screenSize;
 };
 
-export const useBodyClick = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
+export const useBodyClick = (onClose: () => void) => {
     const ref = useRef<HTMLDivElement>(null);
 
-    const onBodyClick = (event: MouseEvent) => {
-        if (ref.current && ref.current.contains(event.target as Node)) {
-            return;
-        }
+    const onBodyClick = useCallback(
+        (event: MouseEvent) => {
+            if (ref.current?.contains(event.target as Node)) {
+                return;
+            }
 
-        setIsOpen(false);
-    };
-
-    useEffect(() => {
-        if (isOpen) {
-            document.body.style.overflow = 'hidden';
-        }
-
-        return () => {
-            document.body.style.overflow = 'auto';
-        };
-    }, [isOpen]);
+            onClose();
+        },
+        [onClose],
+    );
 
     useEffect(() => {
-        document.body.addEventListener('click', onBodyClick);
+        const timeout = setTimeout(() => {
+            document.body.addEventListener('click', onBodyClick);
+        }, 0);
 
         return () => {
+            clearTimeout(timeout);
             document.body.removeEventListener('click', onBodyClick);
         };
+    }, [onBodyClick]);
+
+    return {ref};
+};
+
+export const useOpen = (initialValue: boolean) => {
+    const [isOpen, setIsOpen] = useState(initialValue);
+
+    const handleClose = useCallback(() => {
+        setIsOpen(false);
+    }, []);
+
+    const handleOpen = useCallback(() => {
+        setIsOpen(true);
+    }, []);
+
+    const handleToggle = useCallback(() => {
+        setIsOpen(prev => !prev);
     }, []);
 
     return {
-        ref,
         isOpen,
-        setIsOpen,
+        handleClose,
+        handleOpen,
+        handleToggle,
+    };
+};
+
+export const useModal = () => {
+    const {isOpen, handleClose, handleOpen} = useOpen(false);
+
+    const dispatch = useAppDispatch();
+
+    const handleModalClose = useCallback(() => {
+        handleClose();
+        dispatch(decreaseOpenedItems());
+    }, [handleClose, dispatch]);
+
+    const handleModalOpen = useCallback(() => {
+        handleOpen();
+        dispatch(increaseOpenedItems());
+    }, [handleOpen, dispatch]);
+
+    return {
+        isOpen,
+        handleModalClose,
+        handleModalOpen,
     };
 };
