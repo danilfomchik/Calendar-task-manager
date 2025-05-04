5183fac7917a6f1086e8407f376f436dcee54b92
Daniil Fomenko <danil.fomchik@gmail.com>
2025-05-04 19:16:18 +0300

    deleted unnecessary check of screen size

--- src/pages/Main/components/Calendar/DaysList/index.tsx
+++ src/pages/Main/components/Calendar/DaysList/index.tsx
@@ -1,21 +1,18 @@
 import {useSelector} from 'react-redux';
 
 import {selectMonth, selectYear} from '@/redux/date/selectors';
-import {useScreenSize} from '@/services/hooks';
 import {generateDatesArray} from '@/services/utils';
 
 import Day from '../Day';
 
 const DaysList = () => {
-    const screenSize = useScreenSize();
     const year = useSelector(selectYear);
     const month = useSelector(selectMonth);
 
     const dates = generateDatesArray(year as string, month as string);
 
     return (
-        <div
-            className={`grid grid-rows-6 grid-cols-7 gap-[2px] w-full sm:h-full ${screenSize === 'xs' ? 'h-[calc(100vh-250px)]' : ''}`}>
+        <div className="grid grid-rows-6 grid-cols-7 gap-[2px] w-full h-[calc(100vh-250px)] sm:h-full">
             {dates.map(date => (
                 <Day key={date} date={date} />
             ))}
