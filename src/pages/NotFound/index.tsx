3bfb259b304b7127be7f42cf574ad601dcc19c6f
danilfomchik <danil.fomchik@gmail.com>
2026-01-31 02:23:25 +0200

    added check for correct date for day page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,6 @@
 import {Link} from 'react-router';
 
-const NotFound = () => {
+const NotFound = ({customText}: {customText?: string}) => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <div className="flex items-center gap-3">
@@ -8,7 +8,7 @@ const NotFound = () => {
 
         <div className="self-stretch my-2 border-t border-s border-gray-200 max-md:w-[40%] max-md:mx-auto"></div>
 
-        <p className="text-3xl">PAGE NOT FOUND</p>
+        <p className="text-3xl">PAGE NOT FOUND {customText && `(${customText})`}</p>
       </div>
 
       <Link to="/" className="mt-4 text-2xl transition-all hover:text-sky-500">
