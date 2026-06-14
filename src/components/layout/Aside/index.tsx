c003a3aaf656496caf627ca759fd9cf8a7a488f5
danilfomchik <danil.fomchik@gmail.com>
2026-06-14 10:41:23 +0300

    wip

--- src/components/layout/Aside/index.tsx
+++ src/components/layout/Aside/index.tsx
@@ -1,12 +1,23 @@
+// import {useState} from 'react';
 import {Link} from 'react-router';
 
+import Button from '@/components/common/Button';
 import MonthSmallCalendar from '@/components/common/MonthSmallCalendar';
+import SidebarIcon from '@/components/ui/icons/SidebarIcon';
 
 import MyCalendarsList from './components/MyCalendarsList';
 
 const Aside = () => {
+  // const [isOpen, setIsOpen] = useState(false);
+
   return (
     <aside className="flex-[0_0_240px] py-5 border-r border-secondary-background-color divide-y-[1px] divide-secondary-background-color bg-[#080808] max-lg:hidden">
+      {/* <SidebarIcon /> */}
+      <Button
+        className="p-0 bg-transparent border-none transition-all"
+        startIcon={<SidebarIcon className="size-5" />}
+      />
+
       <div className="flex items-center justify-between gap-3 px-4 pb-5">
         <Link to="/" className="text-lg text-white font-medium">
           Calendar
---
+++ src/components/ui/icons/SidebarIcon.tsx
@@ -0,0 +1,13 @@
+const SidebarIcon = ({className = ''}) => {
+  return (
+    <svg fill="none" viewBox="0 0 16 16" role="presentation" className={className}>
+      <path
+        fill="currentcolor"
+        fillRule="evenodd"
+        d="M14 2.5a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5h-2v-11zm-3.5 0v11H2a.5.5 0 0 1-.5-.5V3a.5.5 0 0 1 .5-.5zM16 3a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zM8.03 7.47l-2.5-2.5-1.06 1.06L6.44 8 4.47 9.97l1.06 1.06 2.5-2.5a.75.75 0 0 0 0-1.06"
+        clipRule="evenodd"></path>
+    </svg>
+  );
+};
+
+export default SidebarIcon;
